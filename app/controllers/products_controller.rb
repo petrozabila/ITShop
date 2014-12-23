b381e26a3061df7bc72b833045d7fdd27baaf0d8
PetroZabila <Zabilamailbox@mail.ru>
2014-12-23 22:35:09 +0400

    with cart lesson

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,24 @@
 class ProductsController < ApplicationController
+  
   def index
     @products = Product.paginate(:page => params[:page], :per_page => 7)
     @rubrics = Rubric.all
+  end
 
+  def add_to_cart
+    product_id = params[:id]
+
+    if cookies[:cart].present?
+      products = cookies[:cart].split(',')
+      products << product_id
+      cookies[:cart] = products.join(',')
+
+    else
+      cookies[:cart] = product_id
+    end
+
+    redirect_to :back
+    
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
 
   resources :rubrics
 
-  resources :products
+  resources :products do
+    member do
+      post 'add_to_cart'
+    end
+  end
 
   root 'products#index'
 
