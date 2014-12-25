52dd32cb1ba1ee6bf7d45f9a2e505c533516efd4
PetroZabila <Zabilamailbox@mail.ru>
2014-12-25 17:33:51 +0400

    cart fix for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
-Rubric.delete_all
 
-Rubric.create(name:'Cars', id:1)
-Rubric.create(name:'Bikes', id:2)
-Rubric.create(name:'Boats', id:3)
+
+Rubric.create(name:'Cars', position:1)
+Rubric.create(name:'Bikes', position:2)
+Rubric.create(name:'Boats', position:3)
+
+
+
+Product.create(title: 'Drundulet', price: 99)
+Product.create(title: 'Tarantas', price: 120)
+Product.create(title: 'Kolymaga', price: 55)
+
+
+
+
